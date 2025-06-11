3ee3625e28243b70534a0e67309e5e1144cc357d
Wright Gichana <wrightgichana@gmail.com>
2025-06-11 17:01:13 +0300

    First commit

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -1,4 +1,6 @@
 // API controller Function to Manage Clerk User with database
 // http://localhost:4000/api/user/webhooks
 
-const clerkWebhooks = async (req,res)
+const clerkWebhooks = async (req,res)=>{
+    
+}

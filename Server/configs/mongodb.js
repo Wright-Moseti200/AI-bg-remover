072ba35fbafc0a29d9209945f80820ab9004984d
Wright Gichana <wrightgichana@gmail.com>
2025-06-18 15:49:24 +0300

    changes

--- Server/configs/mongodb.js
+++ Server/configs/mongodb.js
@@ -1,10 +1,15 @@
 let mongoose = require("mongoose");
 
-const connectDB = async()=>{
-    mongoose.connection.on('connected',()=>{
+const connectDB = async () => {
+    mongoose.connection.on('connected', () => {
         console.log("Database Connected");
     });
-    await mongoose.connect(`${process.env.MONGODB_URI}/bg-removal`);
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+    } catch (err) {
+        console.error("MongoDB connection error:", err);
+        process.exit(1);
+    }
 }
 
-module.exports = connectDB
+module.exports = connectDB;

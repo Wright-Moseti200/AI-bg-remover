b5d15f518ad5f1381c3f8a439a74c1e95f653214
Wright Gichana <wrightgichana@gmail.com>
2025-06-18 15:03:05 +0300

    changes

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -1,64 +1,63 @@
 // API controller Function to Manage Clerk User with database
 // http://localhost:4000/api/user/webhooks
-import {Webhook} from 'svix';
-import userModel from '../models/userModel';
-const clerkWebhooks = async (req,res)=>{
-    try{
-        // Create a svix instance with clerk webhook secret
+const { Webhook } = require('svix');
+const userModel = require('../models/userModel');
+
+const clerkWebhooks = async (req, res) => {
+    try {
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
-        await whook.verify(JSON.stringify(req.body),{
-            "svix-id":req.header["svix-id"],
-            "svix-timestamp":req.headers["svix-timestamp"],
-            "svix-signature":req.header["svix-signature"]
+        await whook.verify(JSON.stringify(req.body), {
+            "svix-id": req.headers["svix-id"],
+            "svix-timestamp": req.headers["svix-timestamp"],
+            "svix-signature": req.headers["svix-signature"]
         });
 
-        const [data,type] = req.body;
+        const [data, type] = req.body;
 
-        switch (type){
-            case "user.created":{
+        switch (type) {
+            case "user.created": {
                 const userData = {
-                    clerkId:data.id,
-                    email:data.email_addresses[0].email_address,
-                    firstName:data.first_name,
-                    lastName:data.lat_name,
-                    photo:data.image_url
+                    clerkId: data.id,
+                    email: data.email_addresses[0].email_address,
+                    firstName: data.first_name,
+                    lastName: data.last_name,
+                    photo: data.image_url
                 }
                 await userModel.create(userData);
-                res.json({})
+                res.json({});
                 break;
             }
 
-            case "user.updated":{
+            case "user.updated": {
                 const userData = {
-                    email:data.email_addresses[0].email_address,
-                    firstName:data.first_name,
-                    lastName:data.lat_name,
-                    photo:data.image_url
+                    email: data.email_addresses[0].email_address,
+                    firstName: data.first_name,
+                    lastName: data.last_name,
+                    photo: data.image_url
                 }
-
-                await userModel.findOneAndUpdate({clerkid:data.id},userData)
-                res.json({})
+                await userModel.findOneAndUpdate({ clerkId: data.id }, userData);
+                res.json({});
                 break;
             }
 
-            case "user.deleted":{
-                await userModel.findOneAndDelete({clerkId:data.id})
-                res.json({})
+            case "user.deleted": {
+                await userModel.findOneAndDelete({ clerkId: data.id });
+                res.json({});
                 break;
             }
 
             default:
+                res.json({});
                 break;
         }
     }
-    catch(error)
-    {
+    catch (error) {
         console.log(error.message);
         res.json({
-            success:false,
-            message:error.message
-        })
+            success: false,
+            message: error.message
+        });
     }
 }
 
-module.exports=clerkWebhooks;
+module.exports = { clerkWebhooks };
--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -1,8 +1,7 @@
 let express = require("express");
-import {clerkWebhooks} from "../controllers/userController"
+let { clerkWebhooks } = require("../controllers/userController");
 let userRouter = express.Router();
 
+userRouter.post('/webhooks', clerkWebhooks);
 
-userRouter.post('/webhooks',clerkWebhooks);
-
-module.exports=userRouter;
+module.exports = userRouter;

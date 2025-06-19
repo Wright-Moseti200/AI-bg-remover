5b1855c46e1caa55bf2b524169cdb116ffea749d
Wright Gichana <wrightgichana@gmail.com>
2025-06-19 21:03:53 +0300

    changes

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -3,57 +3,59 @@
 let {Webhook}=require("svix");
 const userModel = require("../models/userModel");
 
-const clerkWebhooks = async (req,res)=>{
-    try{
+const clerkWebhooks = async (req, res) => {
+    try {
         // Create a Svix instance with clerk webhook secret
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
-        await whook.verify(JSON.stringify(req.body),{
-            "svix-id":req.headers["svix-id"],
-            "svix-timestamp":req.headers["svix-timestamp"],
-            "svix-signature":req.headers["svix-signature"]
+        // Use rawBody for svix verification
+        await whook.verify(req.rawBody, {
+            "svix-id": req.headers["svix-id"],
+            "svix-timestamp": req.headers["svix-timestamp"],
+            "svix-signature": req.headers["svix-signature"]
         });
 
-        const {data,type}= req.body;
+        const { data, type } = req.body;
 
-        switch (type){
-            case "user.created":{
+        switch (type) {
+            case "user.created": {
                 const userData = {
-                    clerkId:data.id,
-                    email:data.email_addresses[0].email_address,
-                    firstName:data.firstName,
-                    latName:data.lastName,
-                    photo:data.image_url
-                }
+                    clerkId: data.id,
+                    email: data.email_addresses[0].email_address,
+                    firstName: data.firstName,
+                    lastName: data.lastName,
+                    photo: data.image_url
+                };
                 await userModel.create(userData);
-                res.json({});
-            break;
+                res.status(200).json({ success: true });
+                break;
             }
 
-            case "user.updated":{
-                 const userData = {
-                    email:data.email_addresses[0].email_address,
-                    firstName:data.firstName,
-                    latName:data.lastName,
-                    photo:data.image_url
-                }
-                await userModel.findByIdAndUpdate({clerkId:data.id},userData);
-            break;
+            case "user.updated": {
+                const userData = {
+                    email: data.email_addresses[0].email_address,
+                    firstName: data.firstName,
+                    lastName: data.lastName,
+                    photo: data.image_url
+                };
+                await userModel.findOneAndUpdate({ clerkId: data.id }, userData);
+                res.status(200).json({ success: true });
+                break;
             }
 
-            case "user.deleted":{
-                await userModel.findOneAndDelete({clerkId:data.id});
-                res.json({})
-            break;
+            case "user.deleted": {
+                await userModel.findOneAndDelete({ clerkId: data.id });
+                res.status(200).json({ success: true });
+                break;
             }
 
             default:
+                res.status(200).json({});
                 break;
         }
-    }
-    catch (error){
-        console.log(error.message);
-
+    } catch (error) {
+        console.error(error.message);
+        res.status(400).json({ error: error.message });
     }
 }
 
-module.exports=clerkWebhooks;
+module.exports = clerkWebhooks;
--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -1,7 +1,23 @@
 let express = require("express");
 let userRouter = express.Router();
- let clerkWebhooks = require("../controllers/userController");
+let clerkWebhooks = require("../controllers/userController");
 
- userRouter.post("/webhooks",clerkWebhooks);
+// Add raw body parser for Clerk webhooks
+const bodyParser = require('body-parser');
+userRouter.post(
+  "/webhooks",
+  bodyParser.raw({ type: "application/json" }),
+  (req, res, next) => {
+    // Save raw body for svix verification
+    req.rawBody = req.body;
+    try {
+      req.body = JSON.parse(req.body.toString());
+    } catch (e) {
+      return res.status(400).json({ error: "Invalid JSON" });
+    }
+    next();
+  },
+  clerkWebhooks
+);
 
- module.exports = userRouter;
+module.exports = userRouter;

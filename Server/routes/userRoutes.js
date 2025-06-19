88916bb2b6470a665c0e476d0fa76778e45751e0
Wright Gichana <wrightgichana@gmail.com>
2025-06-19 23:40:50 +0300

    changes

--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -1,6 +1,6 @@
 let express = require("express");
 let userRouter = express.Router();
- let clerkWebhooks = require("../controllers/userController");
+ let clerkWebhooks = require("../controllers/userController.js");
 
  userRouter.post("/webhooks",clerkWebhooks);
 

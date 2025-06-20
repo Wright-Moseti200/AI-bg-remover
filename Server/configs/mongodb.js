9b70d54639399ebb562070c9433f6e02d7621522
Wright Gichana <wrightgichana@gmail.com>
2025-06-20 11:24:08 +0300

    mongo changes

--- Server/configs/mongodb.js
+++ Server/configs/mongodb.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGODB_URI);
+  await mongoose.connect(`${process.env.MONGODB_URI}/bgdatabase`);
   console.log('Database connected');
 };
 

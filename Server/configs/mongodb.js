726ef8be68d88258809d7d3a144c1f4645aaddb1
Wright Gichana <wrightgichana@gmail.com>
2025-06-20 15:05:10 +0300

    changes

--- Server/configs/mongodb.js
+++ Server/configs/mongodb.js
@@ -1,8 +1,8 @@
 let mongoose = require("mongoose");
 
-const connectDB = async () =>{
-    await mongoose.connect(`${process.env.MONGODB_URI}/bg`);  
+const connectDB = async()=>{
+    await mongoose.connect(`${process.env.MONGODB_URI}/bg`);
     console.log("Database connected");
 }
 
-module.exports={connectDB};
+module.exports = { connectDB };

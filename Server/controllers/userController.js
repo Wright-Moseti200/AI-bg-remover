ec396bd9b22f4bbf87a2938f131892e0357401b0
Wright Gichana <wrightgichana@gmail.com>
2025-06-20 17:55:13 +0300

    env changes

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -1,22 +1,53 @@
 // API Controller Function to Manage Clerk User with database
 // http://localhost:4000/api/user/webhooks
 
-const { verifyWebhook } = require('@clerk/express/webhooks');
+const { Webhook } = require('svix');
 const User = require('../models/userModel');
 
 const clerkWebhooks = async (req, res) => {
     try {
-        // Verify the webhook using Clerk's built-in function
-        const evt = await verifyWebhook(req);
+        // Get the headers and body
+        const svix_id = req.headers["svix-id"];
+        const svix_timestamp = req.headers["svix-timestamp"];
+        const svix_signature = req.headers["svix-signature"];
+
+        // If there are no headers, error out
+        if (!svix_id || !svix_timestamp || !svix_signature) {
+            return res.status(400).json({ 
+                success: false, 
+                message: 'Error occurred -- no svix headers' 
+            });
+        }
+
+        // Get the body
+        const body = JSON.stringify(req.body);
+
+        // Create a new Svix instance with your webhook secret
+        const wh = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+
+        let evt;
+        // Verify the payload with the headers
+        try {
+            evt = wh.verify(body, {
+                "svix-id": svix_id,
+                "svix-timestamp": svix_timestamp,
+                "svix-signature": svix_signature,
+            });
+        } catch (err) {
+            console.error('Error verifying webhook:', err);
+            return res.status(400).json({ 
+                success: false, 
+                message: 'Error verifying webhook' 
+            });
+        }
 
         // Get the ID and type
         const { id } = evt.data;
         const eventType = evt.type;
 
         console.log(`Received webhook with ID ${id} and event type of ${eventType}`);
-        console.log('Webhook payload:', evt.data);
 
-        // Handle the webhook based on event type
+        // Handle the webhook
         switch (eventType) {
             case "user.created": {
                 const userData = {
@@ -26,7 +57,7 @@ const clerkWebhooks = async (req, res) => {
                     lastName: evt.data.last_name,
                     photo: evt.data.image_url
                 };
-
+                
                 await User.create(userData);
                 console.log('User created in database:', userData.clerkId);
                 break;
@@ -39,7 +70,7 @@ const clerkWebhooks = async (req, res) => {
                     lastName: evt.data.last_name,
                     photo: evt.data.image_url
                 };
-
+                
                 await User.findOneAndUpdate({ clerkId: evt.data.id }, userData);
                 console.log('User updated in database:', evt.data.id);
                 break;
@@ -56,12 +87,17 @@ const clerkWebhooks = async (req, res) => {
                 break;
         }
 
-        return res.send('Webhook received');
-
-    } catch (err) {
-        console.error('Error verifying webhook:', err);
-        return res.status(400).send('Error verifying webhook');
+        return res.status(200).json({ success: true, message: 'Webhook received' });
+        
+    } catch (error) {
+        console.error('Webhook error:', error.message);
+        return res.status(400).json({ 
+            success: false, 
+            message: 'Internal server error' 
+        });
     }
 };
 
-module.exports = { clerkWebhooks };
+module.exports = {
+    clerkWebhooks
+};

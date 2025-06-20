1b07dd20fb919cec0ac1230e8a1ef946c3924c4e
Wright Gichana <wrightgichana@gmail.com>
2025-06-20 15:06:31 +0300

    changes

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -1,7 +1,7 @@
 // API Controller Function to Manage Clerk User with database
 // http://localhost:4000/api/user/webhooks
 let {Webhook}=require("svix");
-const {userModel} = require("../models/userModel.js");
+const {userModel} = require("../models/userModel");
 
 const clerkWebhooks = async (req,res)=>{
     try{

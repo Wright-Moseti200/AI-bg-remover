8c86b78f8f464ec1bb8371af3af12509aa7d3913
Wright Gichana <wrightgichana@gmail.com>
2025-06-20 15:25:56 +0300

    server js changes

--- Server/server.js
+++ Server/server.js
@@ -6,7 +6,8 @@ const {connectDB} = require("./configs/mongodb");
 require("dotenv").config();
 let {userRouter} = require("./routes/userRoutes");
 
-connectDB();
+async function payload(){
+ await connectDB();
 let PORT = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
@@ -22,3 +23,5 @@ app.use("/api/user",userRouter);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+}
+payload()

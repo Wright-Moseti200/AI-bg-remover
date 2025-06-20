5954534723f0cbc59defb96bf6c7b44861902c19
Wright Gichana <wrightgichana@gmail.com>
2025-06-20 16:17:33 +0300

    changes

--- Server/configs/mongodb.js
+++ Server/configs/mongodb.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const connectDB = () => {
-  mongoose.connect(process.env.MONGODB_URI);
+  mongoose.connect(`${process.env.MONGODB_URI}/newbgusers`);
   console.log('Database connected');
 };
 

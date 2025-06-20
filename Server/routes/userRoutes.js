413147e75ffca75ec348ee6df78c15e3a9c02406
Wright Gichana <wrightgichana@gmail.com>
2025-06-20 17:45:01 +0300

    creative changes

--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -1,7 +1,12 @@
 let express = require("express");
 let userRouter = express.Router();
- let {clerkWebhooks} = require("../controllers/userController.js");
+let {clerkWebhooks} = require("../controllers/userController.js");
+import { clerkMiddleware } from '@clerk/express';
 
+
+
+
+userRouter.use(clerkMiddleware());
  userRouter.post("/webhooks",clerkWebhooks);
 
  module.exports = {userRouter};
